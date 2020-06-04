936716c4fb8bc7e38ff430c8bde8928085a57041
Dhruv Patel <starb9979@gmail.com>
2020-06-03 23:59:56 -0400

    minor change

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -8,7 +8,7 @@ import "./SortingVisualizer.css";
 const ANIMATION_SPEED_MS = 0.1;
 
 // Change this value for the number of bars (value) in the array.
-const NUMBER_OF_ARRAY_BARS = 350;
+const NUMBER_OF_ARRAY_BARS = 340;
 
 // This is the main color of the array bars.
 const PRIMARY_COLOR = 'turquoise';
